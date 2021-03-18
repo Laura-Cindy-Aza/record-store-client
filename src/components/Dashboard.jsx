88b38a32e40f724d98210d04e93f78408eeb0436
Azael Alonso Campana <zarzarius@gmail.com>
2021-03-18 16:36:40 +0100

    login started and merged

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,33 +1,37 @@
-import React, { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import { getRecordsAction } from '../store/actions/recordsAction';
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getRecordsAction } from "../store/actions/recordsAction";
 
 function Dashboard() {
-    const dispatch = useDispatch();
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(getRecordsAction());
-    }, []);
+  useEffect(() => {
+    dispatch(getRecordsAction());
+  }, []);
 
-    const recordsData = useSelector((state) => {
-        return state.recordsFromReducer.records
-    });
+  const recordsData = useSelector((state) => {
+    return state.recordsFromReducer.records;
+  });
 
-    return (
-        <div>
-            <h1>Dashboard</h1>
-            <h3>Here you can find all our records</h3>
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <h3>Here you can find all our records</h3>
 
-            {/* Map through the records and display them */}
-            {recordsData.map((record) =>
-                <div className="box">
-                    <img src={record.cover} alt={record.title} className="card-image"></img>
-                    <p className="content">{record.title}</p>
-                    <p className="content">{record.artist}</p>
-                    <p className="content">{record.year}</p>
-                </div>)}
+      {/* Map through the records and display them */}
+      {recordsData.map((record) => (
+        <div className='box'>
+          <img
+            src={record.cover}
+            alt={record.title}
+            className='card-image'></img>
+          <p className='content'>{record.title}</p>
+          <p className='content'>{record.artist}</p>
+          <p className='content'>{record.year}</p>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
 
-export default Dashboard
+export default Dashboard;
