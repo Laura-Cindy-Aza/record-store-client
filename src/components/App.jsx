51b22655f421974206c5a84186891882b765a862
Azael Alonso Campana <zarzarius@gmail.com>
2021-03-18 16:25:16 +0100

    login started and merged

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,3 @@
-<<<<<<< HEAD
-import React from 'react';
-import Navbar from './Navbar';
-import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
-import LandingPage from './LandingPage'
-import Dashboard from './Dashboard'
-import Login from './Login';
-import Signup from './Signup';
-import User from './User';
-import userProfile from './UserProfile';
-
-=======
 import React from "react";
 import Navbar from "./Navbar";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
@@ -19,30 +7,13 @@ import Login from "./Login";
 import Signup from "./Signup";
 import User from "./User";
 import userProfile from "./UserProfile";
->>>>>>> 1042a842ad0f8d27c423f53b078106ff92ed1f41
 
 function App() {
   return (
     <>
-<<<<<<< HEAD
-    <header>
-      <Navbar />
-    </header>
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route path='/dashboard' component={Dashboard} />
-          <Route path='/login' component={Login} />
-          <Route path='/signup' component={Signup} />
-          <Route path='/user' component={User} />
-          <Route path='/userprofile' component={userProfile} />
-      </Switch>
-    </>
-  )
-=======
       <header>
         <Navbar />
       </header>
-
       <Switch>
         <Route exact path='/' component={LandingPage} />
         <Route path='/dashboard' component={Dashboard} />
@@ -53,7 +24,6 @@ function App() {
       </Switch>
     </>
   );
->>>>>>> 1042a842ad0f8d27c423f53b078106ff92ed1f41
 }
 
 export default App;
