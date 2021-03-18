77f3b77c9e3898fc94886a2f79f42c35c90bd1c0
Azael Alonso Campana <zarzarius@gmail.com>
2021-03-18 16:17:17 +0100

    login started

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,39 +1,51 @@
-// import React from 'react';
-// import { useForm } from 'react-hook-form';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+// import { useForm } from "react-hook-form";
 
-// function Login() {
-//     const [formData, setFormData] = useState({email: "", password: ""});
+function Login() {
+  const [formData, setFormData] = useState({ email: "", password: "" });
+  const submitHandler = (e) => {
+    e.preventDefault();
+  };
 
-//     return (
-//         <div>
-//             <form>
-//                 <div>
-//                     <input
-//                         type="email"
-//                         name="email"
-//                         value=""
-//                         placeholder="Email"
-//                         required
-//                     />
-//                 </div>
-//                 <div>
-//                     <input
-//                         type="password"
-//                         name="password"
-//                         value=""
-//                         placeholder="Password"
-//                         required
-//                     />
-//                 </div>
-//                 <div>
-//                     <button onClick={submitHandler}>Log in</button>
-//                 </div>
-//                 <div>
-//                     <p>What? You don't have an acount? Create one here!</p>
-//                 </div>
-//             </form>
-//         </div>
-//     )
-// }
+  return (
+    <>
+      <div className='container is-fluid'>
+        <div style={{ width: "400px", margin: "0 auto", paddingTop: "3rem" }}>
+          <p className='is-size-2'>Nice to see you again!</p>
+          <form>
+            <div>
+              <input
+                type='email'
+                name='email'
+                value=''
+                placeholder='Email'
+                required
+              />
+            </div>
+            <div>
+              <input
+                type='password'
+                name='password'
+                value=''
+                placeholder='Password'
+                required
+              />
+            </div>
+            <div>
+              <button onClick={submitHandler}>Log in</button>
+            </div>
+            <div>
+              <p>
+                What? You don't have an acount? Create one{" "}
+                <Link to='/signup'>here!</Link>
+              </p>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
 
-// export default Login
+export default Login;
