869ce22105500f5184d9078ebd8b67fb80a32a2c
Cindy <Punk.c@hotmail.com>
2021-04-15 14:12:48 +0200

    add all reducers but not working <cartreducer>

--- src/store/reducers/allReducers.js
+++ src/store/reducers/allReducers.js
@@ -2,11 +2,14 @@ import { combineReducers } from "redux";
 import authenticationReducer from "./authReducer";
 import usersReducer from "./usersReducer";
 import recordsReducer from "./recordsReducer";
+import cartReducer from './cartReducer'
 
 const allReducers = combineReducers({
   authFromReducer: authenticationReducer,
   userFromReducer: usersReducer,
   recordsFromReducer: recordsReducer,
+  //cartFromReducer: cartReducer,
+
 });
 
 export default allReducers;
