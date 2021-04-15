ac8d645dc61565db2ddc122978be7d2b14eed9f8
limononoil <63930000+limononoil@users.noreply.github.com>
2021-04-15 15:58:12 +0200

    update Cart component

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -7,20 +7,41 @@ import { decreaseQuantity, increaseQuantity, removeFromCart } from "../store/act
 const Cart = () => {
     return (
         <main>
-            <h1 style={{ marginTop: "1rem" }}>Your Shoping Cart</h1>
-            {/* <div>{renderCartItems} </div> */}
-            <h3>TOTAL</h3>
-          
-            <div>
-                <h4 style={{ margin: "0 auto" }}>
-                    {/* {totalPrice > 0 ? totalPrice : null}{"€"} */}
-                </h4>
-                <div>
-                    <button>
-                        Check out
-              </button>
+            <header className="columns header">
+                <div className="column logo-text">
+                    <h1>Shopping Cart Experience</h1>
                 </div>
-            </div>
+                <div className="column user-info">
+                    <img src="#" alt="profile pic"></img>
+                    <div className="dropdown is-hoverable">
+                        <div className="dropdown-trigger">
+                            Logged in as
+                            <span aria-haspopup="true" aria-controls="dropdown-menu4">
+                                <span className="username">USERNAME</span>
+                                <span className="icon is-small">
+                                    <i className="fa fa-angle-down" aria-hidden="true">
+                                    </i>
+                                </span>
+                            </span>
+                        </div>
+                        <div className="dropdown-menu" id="dropdown-menu4" role="menu">
+                            <div className="dropdown-content">
+                                <a className="dropdown-item">Logout</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </header>
+            <section className="section products-table">
+                <div className="columns table-header">
+                    <div className="column">Image</div>
+                    <div className="column is-half">Product Description</div>
+                    <div className="column">Quantity</div>
+                    <div className="column">Price</div>
+                    <div className="column">Total</div>
+                </div>
+                <div id="shopping_cart"></div>
+            </section>
         </main>
     );
 };
