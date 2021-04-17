d30ab62944fc062b7dde3b09c3cfeee848e5b269
Cindy <Punk.c@hotmail.com>
2021-04-17 23:58:29 +0200

    add cart features

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,115 +1,180 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Dashbar from "./Dashbar";
-
+import {Redirect} from 'react-router-dom'
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 import { useDispatch } from "react-redux";
+import music from '../statics/logo.png'
 import {
+  addToCart,
   decreaseQuantity,
   increaseQuantity,
   removeFromCart,
+  emptyCart,
 } from "../store/actions/cartAction";
 
 const Cart = () => {
-  return (
-    <>
-      <Dashbar />
-      <div className='container'>
-        <header className='columns header'>
-          <div className='column logo-text'>
-            <h1>Shopping Cart</h1>
-          </div>
-          <div className='column user-info'>
-            <img src='./images/avatars/guy.jpg' alt='profilepic' />
-            <div className='dropdown is-hoverable'>
-              <div className='dropdown-trigger'>
-                "Logged in as "
-                <span aria-haspopup='true' aria-controls='dropdown-menu4'>
-                  <span className='username'>USERNAME </span>
-                  <span className='icon is-small'>
-                    <i
-                      className='fa fa-angle-down'
-                      aria-hidden='true'
-                      before='dropdown'>
-                      {" "}
-                    </i>
-                  </span>
-                </span>
+  const [redirect, setRedirect] = useState("");
+  const dispatch = useDispatch();
+
+
+  const cartItems = useSelector((state) => state.cartFromReducer.cartItems);
+
+
+
+
+  const renderCartItems = cartItems.map((record, index) => (
+
+
+    <div className='columns'>
+      <div className='column card '>
+
+        {/* card records header!  */}
+
+      <section className='section products-table '>
+        <div className='columns table-header'>
+          <div className='column '>Cover</div>
+          <div className='column'>Description</div>
+          <div className='column'>Quantity</div>
+          <div className='column'>Price</div>
+          <div className='column'>Total</div>
+        </div>
+
+        {/* card records info */}
+        <div id='shopping-cart'>
+          <div>
+            <div className='columns product-row'>
+              <div className='column product-image'>
+                <figure>
+                <img src={record.cover} alt="cover"/>
+                </figure>
               </div>
 
-              <div class='dropdown-menu' role='menu'>
-                <div class='dropdown-content'>
-                  <a className='dropdown-item' href='logout'>
-                    Logout
-                  </a>
-                </div>
+              <div className='column is-half'>
+                <p>{record.title}</p>
+                <p>{record.artist}</p>
+              </div>
+
+              <div className='column center-container'>
+                <span className='mobile-table-heading'>Quantity </span>
+                <input className='quantity ' id={record.id} type="number" name='quantity' min='0' max='100' value='2'/>
+              </div>
+
+              <div className='column center-container'>
+                <span className='mobile-table-heading'>Price € {record.price}</span>
+              </div>
+
+
+              <div className='column center-container'>
+              <span className='mobile-table-heading'>Price € {record.price}</span>
               </div>
             </div>
           </div>
-        </header>
 
-        {/* end of top bar  */}
+        <section className='columns product-row'>
 
-        {/* start of main component, records and stuff  */}
-        <section className='section products-table'>
-          <div className='columns table-header'>
-            <div className='column'>Image</div>
-            <div className='column is-half'>Description</div>
-            <div className='column'>Quantity</div>
-            <div className='column'>Price</div>
-            <div className='column'>Total</div>
-          </div>
 
-          <div className='shopping_cart'>
-            <div>
-              <div className='columns product-row'></div>
-              <div className='columns product-row'></div>
-              <section className='columns checkout'>
-                <div className='column discount'>
-                  <h2>Discount Code or Voucher</h2>
-                  <p>
-                    Type here your discount code. Multiple vouchers cannot be
-                    applied.{" "}
-                  </p>
-                  <div className='field has-addons'>
-                    <div className='control'>
-                      <input className='input' type='text'></input>
-                    </div>
-                    <div className='control'>
-                      <button className='btn is-link primary'>Redeem</button>
-                    </div>
-                  </div>
-                </div>
-                <div className='column submit-form'>
-                  <div id='total'>
-                    <span className='total'></span>
-                  </div>
-                  <div className='field is-grouped'>
-                    <p className='control'>
-                      <button className='button is-dark'>
-                        Back to Dashboard
-                      </button>
-                    </p>
-                    <p className='control'>
-                      <button className='button is-link' href='#'>
-                        Continue to checkout
-                      </button>
-                    </p>
-                  </div>
-                </div>
-              </section>
+          <div className='column submit-form'>
+            <div className='total'>
+              <span className='total'>
+                Total ${record.price}
+              </span>
+            </div>
+
+            <div className='field is-grouped'>
+              <p className='control'>
+                <button className='button is-dark'>Back to Shop</button>
+              </p>
+              <p className='control'>
+                <button className='button is-link'>Continue to checkout</button>
+              </p>
             </div>
           </div>
         </section>
-        {/* end of shopping cart  */}
 
-        {/* start of FOOOOTTEEERRR */}
-        <footer className='footer'>
-          <p className='has-text-centered'>
-            Copyright &copy; 2021. All Rights Reserved
-          </p>
-        </footer>
+
+        </div>
+      </section>
+
+
+
+      {/* closing column divs => */}
       </div>
+    </div>
+  ));
+  const totalPrice = cartItems
+    .map((record) => parseFloat(record.price) * parseFloat(record.quantity))
+    .reduce((acc, num) => acc + num, 0);
+
+
+  return (
+    <>
+       <div className='container is-fluid is-mobile'>
+        <p className='is-size-2'>This is your cart</p>
+        <div className='columns'>
+            {/* start of inputs  */}
+            <div className='column is-half is-vcentered  '>
+              <div className='control'>
+                  <div className='control'>
+                      {renderCartItems}
+                  </div>
+              </div>
+
+              <div className='is-flex is-centered'>
+                <button
+                  className=' button is-primary  mt-5'
+                  //onClick={submitHandler}
+                  >Go Back to Dashboard
+                </button>
+                {redirect === "wrong" && (
+                  <p className='has-text-danger'>You flipped! Try again</p>
+                )}
+              </div>
+          </div>
+
+          {/* start of second column  */}
+          <div className='column is-two-fifths is-vcentered '>
+            <div className='box with-border'>
+                <div className='is-flex'>
+                  <figure className='image is-128x128 is-centered'>
+                      <img className='is-centered is-rounded' src={music} alt='music' />
+                  </figure>
+                </div>
+
+            <div className='card-content'>
+              <div className='media'>
+                <div className="media-left">
+
+              </div>
+
+              <div className='media-content has-text-centered	 '>
+                <p className='title is-4'>Order Total </p>
+                <p className='title is-2'> 55{totalPrice > 0 ? totalPrice : null}
+                {"€"}
+
+                </p>
+              <button className='button is-primary mb-2'>Go to Checkout!</button>
+
+              <p className="has-text-black" id="hover">Place your order now and get a
+              <span className="tag is-danger"> Michi </span> for free!</p>
+
+
+              </div>
+                </div>
+            </div>
+
+
+
+
+
+            {/* last 2 divs  */}
+            </div>
+          </div>
+          {/* end of second column  */}
+        </div>
+      </div>
+
+
     </>
   );
 };
